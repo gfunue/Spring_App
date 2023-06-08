3b155e1a1b55b77000d6bc88445b60487f85db69
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-07 20:39:06 -0500

    Spring initializr file

---
+++ securecapita/src/test/java/com/ngemba/securecapita/SecurecapitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngemba.securecapita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecapitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
