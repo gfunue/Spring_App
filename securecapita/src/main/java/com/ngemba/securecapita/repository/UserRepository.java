68ccc9eaec36540078c485fcfe3edc2e92efcc79
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-08 21:26:34 -0500

    Update UserRepository.java

--- securecapita/src/main/java/com/ngemba/securecapita/repository/UserRepository.java
+++ securecapita/src/main/java/com/ngemba/securecapita/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.ngemba.securecapita.repository;
 
-public interface UserRepository {
+import com.ngemba.securecapita.domain.User;
+
+import java.util.Collection;
+
+public interface UserRepository <T extends User>{
+//    Basic CRUD Operations
+    T create(T data);
+    Collection<T> List(int page, int pageSize);
+    T get(Long id);
+    T update(T data);
+    Boolean delete(Long id);
 }
