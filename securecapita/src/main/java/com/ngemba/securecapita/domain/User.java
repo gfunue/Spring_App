ed609208e48979d25fa310c5a8be9e682814652d
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-08 21:38:06 -0500

    Update User.java

--- securecapita/src/main/java/com/ngemba/securecapita/domain/User.java
+++ securecapita/src/main/java/com/ngemba/securecapita/domain/User.java
@@ -1,4 +1,33 @@
 package com.ngemba.securecapita.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(NON_DEFAULT)
 public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String address;
+    private String phone;
+    private String title;
+    private String bio;
+    private String imageUrl;
+    private boolean enabled;
+    private boolean isNotLocked;
+    private boolean isUsingMfa;
+    private LocalDateTime createdAt;
 }
