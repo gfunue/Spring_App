d7fa70e7673ff8a4c65dc5c1793ba1f9a790b642
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-09 10:16:42 -0500

    Update UserRepositoryImpl.java

--- securecapita/src/main/java/com/ngemba/securecapita/repository/implementation/UserRepositoryImpl.java
+++ securecapita/src/main/java/com/ngemba/securecapita/repository/implementation/UserRepositoryImpl.java
@@ -1,22 +1,48 @@
 package com.ngemba.securecapita.repository.implementation;
 
 import com.ngemba.securecapita.domain.User;
+import com.ngemba.securecapita.exception.ApiException;
 import com.ngemba.securecapita.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.Map;
+
+import static com.ngemba.securecapita.query.UserQuery.*;
+import static java.util.Objects.requireNonNull;
+
 @Repository
 @RequiredArgsConstructor
 @Slf4j
 public class UserRepositoryImpl implements UserRepository<User> {
+
     private final NamedParameterJdbcTemplate jdbc;
+
     @Override
-    public User create(User data) {
+    public User create(User user) {
         // Check if email is unique
+        if (getEmailCount(user.getEmail().trim().toLowerCase()) > 0)
+            throw new ApiException("Email already in use. Please use a different email");
         // Save new user
+        try {
+            KeyHolder holder = new GeneratedKeyHolder();
+            SqlParameterSource parameters = getSqlParameterSource(user);
+            jdbc.update(INSERT_USER_QUERY, parameters, holder);
+            user.setId(requireNonNull(holder.getKey()).longValue());
+            roleRepository.addRoleToUser(user.getId(), ROLE_USER.name());
+        } catch (EmptyResultDataAccessException exception){
+
+        }catch (Exception exception){
+
+        }
+
         // Add role to user
         // Send verification url
         // Save url in verification table
@@ -45,4 +71,12 @@ public class UserRepositoryImpl implements UserRepository<User> {
     public Boolean delete(Long id) {
         return null;
     }
+
+    private Integer getEmailCount(String email) {
+        return jdbc.queryForObject(COUNT_USER_EMAIL_QUERY, Map.of("email", email), Integer.class);
+    }
+
+    private SqlParameterSource getSqlParameterSource(User user) {
+    }
+
 }
