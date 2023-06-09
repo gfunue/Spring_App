bb1853779732781e565a5be15a58be48fa477335
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-08 22:47:44 -0500

    Update UserRepositoryImpl.java

--- securecapita/src/main/java/com/ngemba/securecapita/repository/implementation/UserRepositoryImpl.java
+++ securecapita/src/main/java/com/ngemba/securecapita/repository/implementation/UserRepositoryImpl.java
@@ -2,12 +2,27 @@ package com.ngemba.securecapita.repository.implementation;
 
 import com.ngemba.securecapita.domain.User;
 import com.ngemba.securecapita.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
-
+@Repository
+@RequiredArgsConstructor
+@Slf4j
 public class UserRepositoryImpl implements UserRepository<User> {
+    private final NamedParameterJdbcTemplate jdbc;
     @Override
     public User create(User data) {
+        // Check if email is unique
+        // Save new user
+        // Add role to user
+        // Send verification url
+        // Save url in verification table
+        // Send email to user with verification url
+        // Return the newly created user
+        // If any errors, throw exception message
         return null;
     }
 
