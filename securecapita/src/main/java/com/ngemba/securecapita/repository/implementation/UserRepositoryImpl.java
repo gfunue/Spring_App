df77fef7d0e763337e9cd2eef0b501b2dafbd77a
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-08 21:48:19 -0500

    Create UserRepositoryImpl.java

---
+++ securecapita/src/main/java/com/ngemba/securecapita/repository/implementation/UserRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.ngemba.securecapita.repository.implementation;
+
+import com.ngemba.securecapita.domain.User;
+import com.ngemba.securecapita.repository.UserRepository;
+
+import java.util.Collection;
+
+public class UserRepositoryImpl implements UserRepository<User> {
+    @Override
+    public User create(User data) {
+        return null;
+    }
+
+    @Override
+    public Collection<User> List(int page, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public User get(Long id) {
+        return null;
+    }
+
+    @Override
+    public User update(User data) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+}
