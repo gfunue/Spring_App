91076bee85899c5db08e67b35e7c61c8af7d7142
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-09 10:32:56 -0500

    Update RoleRepository.java

--- securecapita/src/main/java/com/ngemba/securecapita/repository/RoleRepository.java
+++ securecapita/src/main/java/com/ngemba/securecapita/repository/RoleRepository.java
@@ -1,4 +1,17 @@
 package com.ngemba.securecapita.repository;
 
-public interface RoleRepository {
+import com.ngemba.securecapita.domain.Role;
+import com.ngemba.securecapita.domain.User;
+
+import java.util.Collection;
+
+public interface RoleRepository <T extends Role>{
+    //    Basic CRUD Operations
+    T create(T data);
+    Collection<T> List(int page, int pageSize);
+    T get(Long id);
+    T update(T data);
+    Boolean delete(Long id);
+
+    void addRoleToUser(Long userId, String roleName);
 }
