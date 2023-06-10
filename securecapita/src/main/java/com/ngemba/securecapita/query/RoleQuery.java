c7f18b7fe914ea3c48a93257e2522b92119919d9
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-10 18:23:32 -0500

    Update RoleQuery.java

--- securecapita/src/main/java/com/ngemba/securecapita/query/RoleQuery.java
+++ securecapita/src/main/java/com/ngemba/securecapita/query/RoleQuery.java
@@ -2,8 +2,7 @@ package com.ngemba.securecapita.query;
 
 public class RoleQuery {
     public static final String SELECT_ROLE_BY_NAME_QUERY =
-            "INSERT INTO UserRoles (User_id, role_id" +
-                    "VALUES (:userId, :roleId";
-    public static final String INSERT_ROLE_TO_USER_QUERY =
             "SELECT * FROM Roles WHERE name = :name";
+    public static final String INSERT_ROLE_TO_USER_QUERY =
+            "INSERT INTO UserRoles (User_id, role_id) VALUES (:userId, :roleId)";
 }
