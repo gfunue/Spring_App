0dd29039ef8dffc42659f2ca799f3104da366aa5
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-10 16:18:11 -0500

    Create UserResource.java

---
+++ securecapita/src/main/java/com/ngemba/securecapita/resource/UserResource.java
@@ -0,0 +1,45 @@
+package com.ngemba.securecapita.resource;
+
+import com.ngemba.securecapita.domain.HttpResponse;
+import com.ngemba.securecapita.domain.User;
+import com.ngemba.securecapita.dto.UserDTO;
+import com.ngemba.securecapita.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+
+import java.net.URI;
+
+import static java.time.LocalDateTime.now;
+import static java.util.Map.of;
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping(path = "/user")
+@RequiredArgsConstructor
+public class UserResource {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<HttpResponse> saveUser(@RequestBody @Valid User user){
+        UserDTO userDto = userService.createUser(user);
+        return ResponseEntity.created(getUri()).body(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .data(of("user", userDto))
+                        .message("User created")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .build()
+        );
+    }
+
+    private URI getUri() {
+        return URI.create(ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/user/get/<userId>").toUriString());
+    }
+}
