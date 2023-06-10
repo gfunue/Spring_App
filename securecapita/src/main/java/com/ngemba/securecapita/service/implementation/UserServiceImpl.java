f72b1adc4c315f3068c0e10e0ba556a25d51cf84
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-10 16:18:43 -0500

    Update UserServiceImpl.java

--- securecapita/src/main/java/com/ngemba/securecapita/service/implementation/UserServiceImpl.java
+++ securecapita/src/main/java/com/ngemba/securecapita/service/implementation/UserServiceImpl.java
@@ -1,4 +1,20 @@
 package com.ngemba.securecapita.service.implementation;
 
-public class UserServiceImpl {
+import com.ngemba.securecapita.domain.User;
+import com.ngemba.securecapita.dto.UserDTO;
+import com.ngemba.securecapita.dtomapper.UserDTOMapper;
+import com.ngemba.securecapita.repository.UserRepository;
+import com.ngemba.securecapita.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository<User> userRepository;
+    @Override
+    public UserDTO createUser(User user) {
+        return UserDTOMapper.fromUser(userRepository.create(user));
+    }
 }
