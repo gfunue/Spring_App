ad51242968737309499018f591d588c19da26d00
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-10 16:18:31 -0500

    Update UserQuery.java

--- securecapita/src/main/java/com/ngemba/securecapita/query/UserQuery.java
+++ securecapita/src/main/java/com/ngemba/securecapita/query/UserQuery.java
@@ -3,11 +3,10 @@ package com.ngemba.securecapita.query;
 public class UserQuery {
 
     public static final String INSERT_USER_QUERY =
-            "INSERT INTO Users (first_name, last_name, email, password)" +
-            "VALUES (:firstName, :lastName, :email, :password";
+            "INSERT INTO Users (first_name, last_name, email, password) " +
+                    "VALUES (:firstName, :lastName, :email, :password)";
     public static final String COUNT_USER_EMAIL_QUERY =
             "SELECT COUNT(*) FROM Users WHERE email = :email";
     public static final String INSERT_ACCOUNT_VERIFICATION_URL_QUERY =
-            "INSERT INTO AccountVerifications (user_id, url" +
-                    "VALUES (:user_id, :url";
+            "INSERT INTO AccountVerifications (user_id, url VALUES (:user_id, :url)";
 }
