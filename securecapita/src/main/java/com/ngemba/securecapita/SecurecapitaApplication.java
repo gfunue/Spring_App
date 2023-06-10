92fa8368958b7502b73ad3202d7381c0506e1ab0
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-10 18:23:40 -0500

    Update SecurecapitaApplication.java

--- securecapita/src/main/java/com/ngemba/securecapita/SecurecapitaApplication.java
+++ securecapita/src/main/java/com/ngemba/securecapita/SecurecapitaApplication.java
@@ -8,14 +8,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SecurecapitaApplication {
-private static final int STRENHT = 12;
+private static final int STRENGTH = 12;
 	public static void main(String[] args) {
 		SpringApplication.run(SecurecapitaApplication.class, args);
 	}
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder(STRENHT);
+		return new BCryptPasswordEncoder(STRENGTH);
 	}
 
 }
